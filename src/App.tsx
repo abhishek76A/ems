9df8b0d6e9e4c6d562ad4d6853896fd8f2ee6c68
Your Name <youremail@example.com>
2024-12-16 19:06:32 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,23 +8,15 @@ import {AuthContext} from "./context/AuthProvider.tsx";
 
 const App =() => {
 
-    const AuthData = useContext(AuthContext)
-    console.log(AuthData)
-
-
     const [user, setUser] = useState('')
+    const authData = useContext(AuthContext)
 
-    // useEffect(() => {
-    //     if (!localStorage.getItem('employees') || !localStorage.getItem('admin')) {
-    //         setLocalStorage();
-    //     }
-    // }, []);
 
     const handleLogin =(email,password) =>{
         if(email=='admin@me.com' && password=='123') {
             setUser('admin')
             console.log(user)
-        }else if(email=='user@me.com' && password=='123'){
+        }else if(authData && authData.employees.find((e)=> email ===e.email && e.password == password)){
             setUser('user')
             console.log(user)
 
@@ -39,8 +31,8 @@ const App =() => {
         <>
             {!user?<Login handleLogin={handleLogin}/>:''}
             {user== 'admin'?<AdminDashboard/>:<EmployeeDashboard/>}
-         {/*<EmployeeDashboard/>*/}
-         {/*<AdminDashboard/>*/}
+            {/*<EmployeeDashboard/>*/}
+            {/*<AdminDashboard/>*/}
         </>
     )
 }
